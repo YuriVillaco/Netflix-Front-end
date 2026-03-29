2e6e9d66c52e18543a9896dfe55b8e54e0996be2
YuriVillaco <yurivillaco@hotmail.com>
2026-03-29 15:25:42 -0300

    Add files via upload

---
+++ SiteNetflix/netflix-plataform-main/netflix-plataform-main/catalogo/js/data.js
@@ -0,0 +1,55 @@
+export const categories = [
+  
+    {
+        title: "Animes",
+        items: [
+             {
+                img:  "https://wallpapers.com/images/hd/jujutsu-kaisen-desktop-hdckebmzrv7a8cnd.jpg", top10: true, badge: "Animes", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=ZkCwGpYP3ik"
+            },
+            {
+               img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpb2qsW2OeoK3d2dmuQL2ROVPRUTNIOhEndQ&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=3UiP4GwWNv0"
+            },
+            {
+                img: "https://wallpapers.com/images/hd/black-clover-pictures-t13johxx26puqugm.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=xaXpL3k-qbU"
+            },
+            {
+                img: "https://www.plgcolecionaveis.com.br/noticias_imgs/2528.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=7NEyZlGeM_c"
+            },
+           
+        ]
+    },
+      {
+        title: "Clássicos",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC1I-isqw7kAO531VzZyg-NJp8OvEFs5VBpA&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
+];
